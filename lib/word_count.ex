6e34500103599d9633b2d20add1eca0724e7d342
Eddy <eddy147@gmail.com>
2021-03-27 14:18:52 +0100

    first 3 tests success

---
+++ lib/word_count.ex
@@ -0,0 +1,21 @@
+defmodule WordCount do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+    |> String.split()
+    |> list_to_map(%{})
+
+  end
+
+  def list_to_map([], %{} = m), do: m
+
+  def list_to_map([head | tail], %{} = m) do
+    list_to_map(tail, Map.update(m, head, 1, &(&1 + 1)))
+  end
+
+end
